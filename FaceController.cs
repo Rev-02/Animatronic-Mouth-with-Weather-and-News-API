15d4e593792089af68d0f492f87468b2c3653eca
Rev-02 <oliverward70@gmail.com>
2020-05-23 13:28:10 +0100

    Add new POST

--- FaceController.cs
+++ FaceController.cs
@@ -1,12 +1,68 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Ports;
 using System.Linq;
+using System.Net;
+using System.Speech.Synthesis.TtsEngine;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ttsApp
 {
     class FaceController
     {
+        private static SerialPort port;
+        private bool portSuccesful = false;
+
+        public FaceController(string portName, int baudRate)
+        {
+            port = new SerialPort(portName, baudRate);
+            try { 
+                port.Open();
+                portSuccesful = true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                //throw;
+            }
+        }
+
+        public void writeFace(string mouthPos, int[] colorArray)
+        {
+           
+        }
+
+        public void POST()
+        {
+            if (portSuccesful)
+            {
+                try
+                {
+                    port.WriteLine("120,0:0:0;0:0:0");
+                    Thread.Sleep(1500);
+                    port.WriteLine("0,0:0:0;0:0:0");
+                    string writeString;
+                    int[] colArr = { 0, 0, 0 };
+                    for (int i = 0; i < 3; i++)
+                    {
+                        for (int b = 0; b < 128; b++)
+                        {
+
+                            colArr[i] = b;
+                            writeString = "0" + "," + colArr[0] + ":" + colArr[1] + ":" + colArr[2] + ";" + colArr[0] + ":" + colArr[1] + ":" + colArr[2];
+                            port.WriteLine(writeString);
+                        }
+                        colArr[i] = 0;
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
     }
 }
